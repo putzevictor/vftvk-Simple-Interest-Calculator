747f2b6df449d30da760c97e8e24f7148e85e4c3
putzevictor <80172555+putzevictor@users.noreply.github.com>
2022-02-26 11:52:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+  am = document.getElementById("amount").value;
+  ra = document.getElementById("rate").value;
+  ye = document.getElementById("years").value;
+  const date = new Date();
+  const year = date.getFullYear() + Number(ye);
+
+  document.getElementById("print").innerHTML = "If you deposit <mark>"+am+"</mark>,<br/>at an interest rate of <mark>"+ra+"%</mark>.<br/>You will receive an amount of <mark>"+(am * (ra/100) * ye)+"</mark>,<br/>In the year <mark>"+year+"</mark>";
+
 }
-        
